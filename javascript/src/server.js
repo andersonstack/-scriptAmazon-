65052159afa2df7adff9ace3dae60a23705077ba
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-21 13:47:36 -0300

    feat: adding controls for errors

--- javascript/src/server.js
+++ javascript/src/server.js
@@ -2,29 +2,44 @@ import express from "express";
 import fetchAmazonPage from "./utils/fetchAmazonPage.js";
 import parseHTMLToJson from "./utils/parseHTMLToJson.js";
 import extractProductsFromDOM from "./utils/produtcs.js";
+import {
+  badRequest,
+  notFound,
+  serverError,
+  badGateway,
+} from "./utils/httpErros.js";
 
 const app = express();
 const PORT = 3000;
 
 app.get("/api/scrape", async (req, res) => {
   try {
-    const keyword = req.query.keyword;
+    const keyword = req.query.k;
 
     // Check if keyword is provided
-    if (!keyword) {
-      return res.status(400).json({ error: "Keyword is required" });
-    }
+    if (!keyword) return badRequest(res, "No keyword provided");
 
     // Product link on Amazon store and Request URL
     const html = await fetchAmazonPage(keyword);
+    // Check if product link is found
+    if (!html) return notFound(res, "No products found");
+
     // To parse the HTML to JSON
     const document = parseHTMLToJson(html);
+    // Check if HTML is parsed
+    if (!document) return badGateway(res, "Failed to parse HTML");
+
     // To extract products from DOM
     const products = extractProductsFromDOM(document);
+    // Check if products are extracted
+    if (!products || products.length === 0)
+      return serverError(res, "Failed to extract products");
 
+    // Return products | Sucess
     res.json({ products });
   } catch (e) {
     console.error(e);
+    serverError(res, e.message);
   }
 });
 
