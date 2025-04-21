0b76d7149b262c2a18c9d7d3975a2507e18f2aa0
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-21 13:18:31 -0300

    feat: adding security to require the key

--- javascript/src/server.js
+++ javascript/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fetchAmazonPage from "./utils/fetchAmazonPage.js";
 import parseHTMLToJson from "./utils/parseHTMLToJson.js";
+import extractProductsFromDOM from "./utils/produtcs.js";
 
 const app = express();
 const PORT = 3000;
@@ -9,12 +10,19 @@ app.get("/api/scrape", async (req, res) => {
   try {
     const keyword = req.query.keyword;
 
+    // Check if keyword is provided
+    if (!keyword) {
+      return res.status(400).json({ error: "Keyword is required" });
+    }
+
     // Product link on Amazon store and Request URL
     const html = await fetchAmazonPage(keyword);
     // To parse the HTML to JSON
     const document = parseHTMLToJson(html);
+    // To extract products from DOM
+    const products = extractProductsFromDOM(document);
 
-    res.json({ document });
+    res.json({ products });
   } catch (e) {
     console.error(e);
   }
