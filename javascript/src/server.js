8cd0b396474833964180b57a6af73c716b31ec90
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-21 10:32:59 -0300

    feat: create server with scraping endpoint using express, axios and jsom

--- javascript/src/server.js
+++ javascript/src/server.js
@@ -1,8 +1,31 @@
 import express from "express";
+import { JSDOM } from "jsdom";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
 
-export default app
+app.get("/api/scrape", async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    // Product link on Amazon store and Request URL
+    const amazonUrl = `https://www.amazon.com/s?k=${encodeURIComponent(
+      keyword
+    )}`;
+
+    const { data } = await axios.get(amazonUrl, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ...",
+      },
+    });
+
+    // Creating the DOM
+    const dom = new JSDOM(data);
+    const document = dom.window.document;
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
