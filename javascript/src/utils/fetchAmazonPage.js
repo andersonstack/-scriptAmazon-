b845e7837aa340d188d12e620f0a83c22d6009cf
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-21 13:15:29 -0300

    chore: response adjustment

--- javascript/src/utils/fetchAmazonPage.js
+++ javascript/src/utils/fetchAmazonPage.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 export default async function fetchAmazonPage(keyword) {
   const url = `https://www.amazon.com/s?k=${encodeURIComponent(keyword)}`;
-  const response = await axios.get(url, {
+  const { data } = await axios.get(url, {
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     },
   });
-  return response.data;
+  return data;
 }
