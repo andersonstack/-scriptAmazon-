88aa97913373fe3616e03d86fc8cef07f46df44a
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-22 09:51:55 -0300

    fix: error setup if you don't have the key

--- frontend/src/service/getApi.js
+++ frontend/src/service/getApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const getApi = async (keyword) => {
+  if (!keyword) return;
+
   const response = await axios.get(
     `http://localhost:3000/api/scrape?k=${encodeURIComponent(keyword)}`
   );
